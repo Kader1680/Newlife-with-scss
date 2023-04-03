29fdf71b4761f56e8c47abc40b57e76450631fe7
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-03 21:36:51 +0100

    update navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let bars = document.querySelector(".fa-bars");
+let list = document.querySelector(".list");
+bars.addEventListener("click", () => {
+  list.classList.toggle("blistBlock");
+  // alert('fgfdgdfg')
+});
